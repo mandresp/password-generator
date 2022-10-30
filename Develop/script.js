de963b1e64e086248aab8dfba14579d27e567ec7
[Andres Perez] <mandresperez.uk@gmail.com>
2022-10-30 11:17:39 -0600

    Added in arrays and the prompts for deciding password length and which characters are used

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // Defining arrays
+  const upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  const specialArr = ["!", "@", "#", "$", "%", "&"]
+
+  var userLength =prompt("Enter password length between 8-128 characters.")
+
+  if (userLength < 8 || userLength > 128) {
+    console.clear(userLength);
+    alert("Password is not the right length");
+    generatePassword();
+  } else {
+    console.log(userLength);
+  }
+
+  var upperCase = confirm("Would you like upper case?");
+  var lowerCase = confirm("Would you like lower case?");
+  var numbers = confirm("Would you like numbers?");
+  var special = confirm("Would you like special characters?");
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
